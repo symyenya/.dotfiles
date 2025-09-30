dd6fae80fb1ede228dd298a64d845a44131cb33f
Simon Nya <simon@katzenburschen.de>
2025-09-30 18:27:56 +0200

    Flake Config

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    lanzaboote = {
+      url = "github:nix-community/lanzaboote/v0.4.2";
+
+      # Optional but recommended to limit the size of your system closure.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {self, nixpkgs, lanzaboote, ...}:
+    let
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        nyaOS = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            lanzaboote.nixosModules.lanzaboote
+
+            ({ pkgs, lib, ... }: {
+
+              environment.systemPackages = [
+                # For debugging and troubleshooting Secure Boot.
+                pkgs.sbctl
+              ];
+
+              # Lanzaboote currently replaces the systemd-boot module.
+              # This setting is usually set to true in configuration.nix
+              # generated at installation time. So we force it to false
+              # for now.
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/var/lib/sbctl";
+              };
+            })
+          ];
+        };
+      };
+    };
+}
