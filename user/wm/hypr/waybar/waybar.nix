ee3b089d1938a777dd76d383af067767bbd45a08
Simon Nya <simon@katzenburschen.de>
2025-09-30 20:35:03 +0200

    s

--- user/wm/hypr/waybar/waybar.nix
+++ user/wm/hypr/waybar/waybar.nix
@@ -125,151 +125,57 @@ window#waybar {
 }
                 '';
     settings = {
-        "layer": "top", // Waybar at top layer
-    "position": "top", // Waybar position (top|bottom|left|right)
-    "height": 50, // Waybar height (to be removed for auto height)
-    // "width": 1280, // Waybar width
-    "spacing": 4, // Gaps between modules (4px)
-    // Choose the order of the modules
-    "modules-left": [
-        "hyprland/workspaces"
-    ],
-    "modules-center": [
-	"image",
-        "mpris"
-    ],
-    "modules-right": [
-	    "pulseaudio",
-        "pulseaudio/slider",
-        "hyprland/language",
-        "clock",
-	    "custom/power"
-    ],
-    //Modules configuration
-    "hyprland/workspaces": {
-        "disable-scroll": true,
-        "all-outputs": true,
-        "warp-on-scroll": false,
-        "format": "{name}",
-        "format-icons": {
-            "urgent": "",
-            "active": "",
-            "default": ""
-        }
-    },
-    "idle_inhibitor": {
-        "format": "{icon}",
-        "format-icons": {
-            "activated": "",
-            "deactivated": ""
-        }
-    },
-    "pulseaudio": {
-    "format": "{volume}% {icon}",
-    "format-bluetooth": "{volume}% {icon}",
-    "format-muted": "",
-    "format-icons": {
-        "alsa_output.pci-0000_00_1f.3.analog-stereo": "",
-        "alsa_output.pci-0000_00_1f.3.analog-stereo-muted": "",
-        "headphone": "",
-        "hands-free": "",
-        "headset": "",
-        "phone": "",
-        "phone-muted": "",
-        "portable": "",
-        "car": "",
-        "default": ["", ""]
-    },
-    "scroll-step": 1,
-    "on-click": "pavucontrol",
-    "ignored-sinks": ["Easy Effects Sink"]
-},
-    "pulseaudio/slider": {
-	"format":"{}",
-        "min": 0,
-    	"max": 100,
-    	"orientation": "horizontal"
-    },
-    "image": {
-     "exec":"~/.config/waybar/custom/spotify/album_art.sh",
-     "size": 36,
-     "interval": 30 
-    },
-    "mpris": {
-	"format": "{title} - {artist}",
-	"format-paused": "{status_icon} - {title} - {artist}",
-	"player-icons": {
-		"default": "▶",
-		"mpv": "🎵"
-	},
-	"interval": 2,
-	"status-icons": {
-		"paused": "⏸"
-	},
-	"ignored-players": ["firefox"]
-    },
-    "network": {
-        "format-wifi": "   {essid} ({signalStrength}%)",
-        "format-ethernet": "{ipaddr}/{cidr} ",
-        "tooltip-format": "{ifname} via {gwaddr} ",
-        "format-linked": "{ifname} (No IP) ",
-        "format-disconnected": "Disconnected ⚠",
-	"on-click": "sh ~/scripts/rofi-wifi-menu/rofi-wifi-menu.sh"
-	 
-    },
-    "cpu": {
-        "format": "  {usage}%",
-        "tooltip": true
-    },
-    "memory": {
-        "format": "  {}%",
-	"tooltip": true
-    },
-    "temperature": {
-        "interval": 10,
-        "hwmon-path": "/sys/class/hwmon/hwmon3/temp1_input",
-        "critical-threshold": 100,
-        "format-critical": " {temperatureC}",
-        "format": " {temperatureC}°C"
-    },
-    "hyprland/language": {
-       "format": "  {}",
-       "format-en": "EN",
-       "format-ru": "RU"
-    },
-    "clock": {
-        "format": "{:%H:%M | %e %B} ",
-        "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>",
-        "format-alt": "{:%Y-%m-%d}",
-        "calendar": {
-                    "mode"          : "year",
-                    "mode-mon-col"  : 3,
-                    "weeks-pos"     : "right",
-                    "on-scroll"     : 1,
-                    "format": {
-                              "months":     "<span color='#ffead3'><b>{}</b></span>",
-                              "days":       "<span color='#c0caf5'><b>{}</b></span>",
-                              "weeks":      "<span color='#99ffdd'><b>W{}</b></span>",
-                              "weekdays":   "<span color='#ffcc66'><b>{}</b></span>",
-                              "today":      "<span color='#ff6699'><b><u>{}</u></b></span>"
-                              }
-                    },
-        "actions":  {
-                    "on-scroll-up": "shift_up",
-                    "on-scroll-down": "shift_down"
-                    }
-    },
-    "custom/power": {
-        "format" : "⏻ ",
-		"tooltip": false,
-		"menu": "on-click",
-		"menu-file": "$HOME/.config/waybar/custom/power_menu.xml",
-		"menu-actions": {
-			"lock": "hyprlock",
-			"shutdown": "shutdown",
-			"reboot": "reboot"
-		}
-    }
-    }
+        mainBar = {
+            layer= "top";
+            position= "top";
+            height= 50;
+            spacing= 4;
+            modules-left= [
+                "hyprland/workspaces"
+            ];
+            modules-center= [
+	            "image"
+                "mpris"
+            ],
+            modules-right = [
+	            "pulseaudio"
+                "pulseaudio/slider"
+                "hyprland/language"
+                "clock"
+	            "custom/power"
+            ];
+
+            "hyprland/workspaces"= {
+                disable-scroll= true;
+                all-outputs= true;
+                warp-on-scroll= false;
+                format="{name}";
+                format-icons= {
+                    urgent= "";
+                    active= "";
+                    default= "";
+                };
+            };
+
+            pulseaudio= {
+                format= "{volume}% {icon}";
+                format-bluetooth= "{volume}% {icon}";
+                format-muted= "";
+                format-icons= {
+                    alsa_output.pci-0000_00_1f.3.analog-stereo= "";
+                    alsa_output.pci-0000_00_1f.3.analog-stereo-muted= "";
+                    headphone= "";
+                    hands-free= "";
+                    headset= "";
+                    phone= "";
+                    phone-muted= "";
+                    portable= "";
+                    car= "";
+                    default= [""; ""];
+                };
+                scroll-step= 1;
+                on-click= "pavucontrol";
+                ignored-sinks= ["Easy Effects Sink"];
+            };
     };
 }
